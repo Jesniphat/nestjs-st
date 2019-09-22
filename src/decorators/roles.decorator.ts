9b07a6970dceb00301ebad48c901e0f426b5909b
Jesniphat <jesniphat@hotmail.com>
2019-09-22 15:05:55 +0700

    Update roles.decorator.ts

--- src/decorators/roles.decorator.ts
+++ src/decorators/roles.decorator.ts
@@ -1,3 +1,10 @@
 import { ReflectMetadata } from '@nestjs/common';
 
 export const Roles = (...roles: string[]) => ReflectMetadata('roles', roles);
+
+
+// NEW VERSION USE
+
+// import { SetMetadata } from '@nestjs/common';
+
+// export const Roles = (...roles: string[]) => SetMetadata('roles', roles);
